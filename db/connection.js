9c4643ab5802c00e1e3cf08be9789129b8fc1d5c
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-02 21:15:07 -0400

    added dotenv for mysql password

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,14 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      // Your MySQL username,
       user: 'root',
-      // Your MySQL password
-      password: 'Summer.2021',
+      password: process.env.PASSWORD,
       database: 'employees'
-    },
-    console.log('Connected to the employees database.')
+    }
   );
 
 module.exports = db;
